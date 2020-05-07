b632a91f54a9cdf47d2221a0b384af79e4ec5235
n-peugnet <n.peugnet@free.fr>
2020-05-07 20:10:38 +0200

    add start stop commands

--- TP-06/EXO-04/taskmonitor.c
+++ TP-06/EXO-04/taskmonitor.c
@@ -16,8 +16,13 @@ module_param(target, int, 0660);
 struct task_monitor {
 	struct pid *pid;
 };
+struct task_sample {
+	u64 utime;
+	u64 stime;
+};
 
 static struct task_monitor task_monitor;
+static struct task_sample task_sample;
 static struct task_struct *monitor_thread;
 static u_int16_t seconds = 5;
 
@@ -46,35 +51,64 @@ static int monitor_fn(void *unused)
 	struct task_struct *task;
 
 	while (!kthread_should_stop()) {
-		set_current_state(TASK_UNINTERRUPTIBLE);
-		schedule_timeout(seconds * HZ);
-		if (task_monitor.pid == NULL)
-			continue;
 		task = get_pid_task(task_monitor.pid, PIDTYPE_PID);
 		if (pid_alive(task)) {
-			pr_info("pid %d usr %lld sys %lld", task->pid,
-				task->utime / 1000000, task->stime / 1000000);
+			task_sample.utime = task->utime;
+			task_sample.stime = task->stime;
 		}
 		put_task_struct(task);
+		set_current_state(TASK_UNINTERRUPTIBLE);
+		schedule_timeout(seconds * HZ);
 	}
+	monitor_thread = NULL;
 	return 0;
+}
+
+static int monitor_start(void)
+{
+	if (monitor_thread) {
+		pr_info("Thread already running\n");
+		return 1;
+	}
+	monitor_pid(target);
+	if (task_monitor.pid) {
+		monitor_thread = kthread_run(monitor_fn, NULL, "monitor_fn");
+		pr_info("Thread started, pid: %d\n", monitor_thread->pid);
+		return 0;
+	}
+	pr_info("Could not start thread: task %d does not exist\n", target);
+	return 1;
+}
+
+static int monitor_stop(void)
+{
+	if (monitor_thread) {
+		kthread_stop(monitor_thread);
+		pr_info("Thread stopped\n");
+		return 0;
+	}
+	pr_info("No thread running\n");
+	return 1;
 }
 
 static ssize_t taskmonitor_show(struct kobject *kobj,
 				struct kobj_attribute *attr, char *buf)
 {
-	return snprintf(buf, 16, "target: %d\n", target);
+	return snprintf(buf, 30, "pid %d usr %lld sys %lld\n", target,
+			task_sample.utime / 1000000,
+			task_sample.stime / 1000000);
 }
 
 static ssize_t taskmonitor_store(struct kobject *kobj,
 				 struct kobj_attribute *attr, const char *buf,
 				 size_t count)
 {
-	switch (*buf) {
-	case 'r':
-	default:
-		monitor_pid(target);
-		break;
+	if (!strncmp(buf, "start", 5)) {
+		pr_info("Run command start\n");
+		monitor_start();
+	} else if (!strncmp(buf, "stop", 4)) {
+		pr_info("Run command stop\n");
+		monitor_stop();
 	}
 	return count;
 }
@@ -88,16 +122,14 @@ static int taskmonitor_init(void)
 	err = sysfs_create_file(kernel_kobj, &hello_kobj.attr);
 	if (err)
 		return err;
-	monitor_pid(target);
-	monitor_thread = kthread_run(monitor_fn, NULL, "monitor_fn");
+	monitor_start();
 	pr_info("taskmonitor module loaded\n");
 	return 0;
 }
 
 static void taskmonitor_exit(void)
 {
-	if (monitor_thread)
-		kthread_stop(monitor_thread);
+	monitor_stop();
 	sysfs_remove_file(kernel_kobj, &hello_kobj.attr);
 	put_current_pid();
 	pr_info("taskmonitor module unloaded\n");
